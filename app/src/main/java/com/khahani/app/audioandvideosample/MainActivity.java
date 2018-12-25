c9e62007eeacefb66d575842a5e9289ec5c62031
Mohammadreza khahani <mr.khahani@gmail.com>
2018-12-26 00:42:26 +0330

    1

--- app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
+++ app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
@@ -1,13 +1,37 @@
 package com.khahani.app.audioandvideosample;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer.OnPreparedListener myOnPreparedListener =
+            new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mediaPlayer) {
+            mediaPlayer.start();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource("http://sv.blogmusic.ir/myahang/Classic-music-1.mp3");
+            mediaPlayer.setOnPreparedListener(myOnPreparedListener);
+            mediaPlayer.prepareAsync();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
