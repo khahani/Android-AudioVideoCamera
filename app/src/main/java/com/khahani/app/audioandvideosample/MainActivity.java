a389df3f0eaf957fbe773a13c5eeef2a86a3ac70
Mohammadreza khahani <mr.khahani@gmail.com>
2018-12-26 01:17:02 +0330

    todos 1 to 7 setting up.

--- app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
+++ app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
@@ -8,10 +8,23 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    //todo: 4. Define MediaPlayer.OnPreparedListener and call it myOnPreparedListener
+
+    //todo: 5. inside the onPrepared method of the listener
+    // call mediaPlayer.start method by using mediaPlayer parameter.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //todo: 2. Create an instance of MediaPlayer and call it mediaPlayer
+
+        //todo: 3. Call setDataSource method by your music resource. If there is need to use
+        //          try-catch do it and take care rest of your code.
+
+        //todo: 6. Call onPreparedListener and
+
+        //todo: 7. Call prepareAsync method to prepare media off the UI thread
     }
 }
