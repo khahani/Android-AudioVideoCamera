f7cc0e70e590bc5c1290cdd6224c7cc83ee0906b
Mohammadreza khahani <mr.khahani@gmail.com>
2018-12-26 01:00:52 +0330

    Initial Again For Branching

--- app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
+++ app/src/main/java/com/khahani/app/audioandvideosample/MainActivity.java
@@ -8,30 +8,10 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaPlayer.OnPreparedListener myOnPreparedListener =
-            new MediaPlayer.OnPreparedListener() {
-        @Override
-        public void onPrepared(MediaPlayer mediaPlayer) {
-            mediaPlayer.start();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        try {
-
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            mediaPlayer.setDataSource("http://sv.blogmusic.ir/myahang/Classic-music-1.mp3");
-            mediaPlayer.setOnPreparedListener(myOnPreparedListener);
-            mediaPlayer.prepareAsync();
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 }
